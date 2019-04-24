36faad2ddbc9bca69eaaafe93ce90544dae70723
quicky84 <victor.ermolaev@protonmail.com>
2019-04-24 15:12:54 +0200

    Restructure backend scripts

--- backend/configure/finalize_config.sh
+++ backend/configure/finalize_config.sh
@@ -7,7 +7,7 @@ pubNodes=""
 target=$3
 for i in $(seq 1 $((node_count)))
 do
-    path="../../shared-config/pub_$i.toml"
+    path="../../../shared-config/pub_$i.toml"
     pubNodes="$pubNodes $path"
 done
 echo target/$target/exonum-neo4j finalize --public-api-address 172.17.0.$num:8200 --private-api-address 172.17.0.$num:8091 sec_$node.toml node_$node_cfg.toml --public-configs $pubNodes
--- backend/reConfTestNode.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-node=$1
-target=$2
-
-
-echo target/$target/exonum-neo4j generate-config ../../shared-config/common.toml  ../../shared-config/pub_$node.toml sec_$node.toml --peer-address 172.17.0.$((node+1)):6331
-target/$target/exonum-neo4j generate-config ../../shared-config/common.toml  ../../shared-config/pub_$node.toml sec_$node.toml --peer-address 172.17.0.$((node+1)):6331
