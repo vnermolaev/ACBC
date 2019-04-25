6e70eac98ebd6c85ec01e168d2d074fbc09d7f5e
quicky84 <victor.ermolaev@protonmail.com>
2019-04-25 10:38:07 +0200

    Add navigation

--- explorer/src/App.vue
+++ explorer/src/App.vue
@@ -1,14 +1,55 @@
 <template>
     <v-app>
         <v-content>
-            <router-view></router-view>
+            <v-layout row wrap>
+                <v-flex xs2>
+                    <v-navigation-drawer permanent>
+                        <v-toolbar flat>
+                            <v-list>
+                                <v-list-tile>
+                                    <v-list-tile-title class="title"
+                                        >Application</v-list-tile-title
+                                    >
+                                </v-list-tile>
+                            </v-list>
+                        </v-toolbar>
+                        <v-divider></v-divider>
+                        <v-list dense class="pt-0">
+                            <v-list-tile
+                                v-for="item in items"
+                                :key="item.title"
+                                @click="$router.push(item.route)"
+                            >
+                                <v-list-tile-action>
+                                    <v-icon>{{ item.icon }}</v-icon>
+                                </v-list-tile-action>
+                                <v-list-tile-content>
+                                    <v-list-tile-title>
+                                        {{ item.title }}
+                                    </v-list-tile-title>
+                                </v-list-tile-content>
+                            </v-list-tile>
+                        </v-list>
+                    </v-navigation-drawer>
+                </v-flex>
+                <v-flex xs10>
+                    <router-view></router-view>
+                </v-flex>
+            </v-layout>
         </v-content>
     </v-app>
 </template>
 
-<script>
-export default {
-    name: 'App',
-    components: {},
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component({})
+export default class App extends Vue {
+    name = 'App'
+    items = [
+        { title: 'Send query', icon: 'format_quote', route: 'communicator' },
+        { title: 'Audit history', icon: 'history', route: 'history' },
+    ]
 }
 </script>
