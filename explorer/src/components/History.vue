7316f727dac24e6c1986d1260339a7b5bee7af1f
quicky84 <victor.ermolaev@protonmail.com>
2019-04-25 10:37:24 +0200

    Add dummy history

---
+++ explorer/src/components/History.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-layout>
+        NOTHING YET
+    </v-layout>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component({})
+export default class History extends Vue {}
+</script>
