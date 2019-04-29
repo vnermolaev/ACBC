a6a0342c6c43a7e7c62b292f99897065caa703dd
quicky84 <victor.ermolaev@protonmail.com>
2019-04-29 15:01:41 +0200

    Update env generation

--- explorer/generate_environment.sh
+++ explorer/generate_environment.sh
@@ -2,17 +2,17 @@
 
 node=$1
 ip=$2
+
+echo "WEBSERVER_PORT=3005" > .env
+echo "NEO4J_BOLT_ADDRESS=$ip" >> .env
+echo "NEO4J_BOLT_PORT=768$node" >> .env
+echo "NEO4J_USERNAME=neo4j" >> .env
+echo "NEO4J_PASSWORD=exonumNeo4j" >> .env
+echo "EXONUM_ADDRESS=172.17.0.$((node+1))" >> .env
+echo "EXONUM_PORT=8200" >> .env
+
 prefix="VUE_APP_"
-
-echo "${prefix}WEBSERVER_PORT=3005" > .env
-echo "${prefix}NEO4J_BOLT_ADDRESS=$ip" >> .env
-echo "${prefix}NEO4J_BOLT_PORT=768$node" >> .env
-echo "${prefix}NEO4J_USERNAME=neo4j" >> .env
-echo "${prefix}NEO4J_PASSWORD=exonumNeo4j" >> .env
-echo "${prefix}EXONUM_ADDRESS=172.17.0.$((node+1))" >> .env
-echo "${prefix}EXONUM_PORT=8200" >> .env
-
-# printf 'EXONUM_PRIVATE_KEY=' >> .env
-# grep -Po 'service_secret_key = "\K[^"]*' ../backend/sec_$node.toml >> .env
-# printf 'EXONUM_PUBLIC_KEY=' >> .env
-# grep -Po 'public_key = "\K[^"]*' ../backend/sec_$node.toml >> .env
+printf "${prefix}EXONUM_PRIVATE_KEY=" >> .env
+grep -Po 'service_secret_key = "\K[^"]*' ../backend/sec_$node.toml >> .env
+printf "${prefix}EXONUM_PUBLIC_KEY=" >> .env
+grep -Po 'public_key = "\K[^"]*' ../backend/sec_$node.toml >> .env
