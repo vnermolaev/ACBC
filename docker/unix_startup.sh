d79ae6fd74b7724a5a87114ffba3073e1803c2b3
quicky84 <victor.ermolaev@protonmail.com>
2019-04-29 16:13:10 +0200

    Parametrize password

--- docker/unix_startup.sh
+++ docker/unix_startup.sh
@@ -6,6 +6,7 @@ node_count=4
 api_port=8200
 neo4j_front_port=7470
 bolt_port=7680
+neo4j_password="neo4j"
 frontend_port=3000
 target="release"
 
@@ -16,7 +17,7 @@ do
     echo docker run -t -d -i --name node$i -p 820$i:8200 -p 747$i:7474 -p 768$i:7687 -p 300$i:3005 -v $volume_name:/shared-config $image_name
     docker run -t -d -i --name node$i -p 820$i:8200 -p 747$i:7474 -p 768$i:7687 -p 300$i:3005 -v $volume_name:/shared-config $image_name
     docker exec -w /ACBC/ node$i git pull
-    docker exec node$i neo4j-admin set-initial-password exonumNeo4j
+    docker exec node$i neo4j-admin set-initial-password $neo4j_password
     docker exec node$i neo4j start
 done
 
