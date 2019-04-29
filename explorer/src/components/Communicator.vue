d440ce2b27faaff71166e9e60fc38f96d31fe819
quicky84 <victor.ermolaev@protonmail.com>
2019-04-29 13:58:01 +0200

    Complete components

--- explorer/src/components/Communicator.vue
+++ explorer/src/components/Communicator.vue
@@ -1,6 +1,6 @@
 <template>
     <v-layout row wrap>
-        <v-flex xs8 offset-xs2 class="pt-5">
+        <v-flex xs10 offset-xs1 class="pt-5">
             <v-card>
                 <v-textarea
                     box
@@ -25,8 +25,8 @@
         <v-flex xs8 offset-xs2 class="pt-5">
             <v-card>
                 <v-data-table
-                    :headers="txInfoHeaders"
-                    :items="txProperties"
+                    :headers="txHeaders"
+                    :items="txData"
                     class="elevation-1"
                     hide-actions
                     :loading="loading"
@@ -47,16 +47,23 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
 
 import * as Blockchain from '../api/blockchain'
 
+import { delay } from '../api/convenience'
+
 @Component({})
 export default class Communicator extends Vue {
+    keys: Blockchain.KeyPair = {
+        publicKey: process.env.VUE_APP_EXONUM_PUBLIC_KEY as string,
+        secretKey: process.env.VUE_APP_EXONUM_PRIVATE_KEY as string,
+    }
+
     cypher: string =
         'CREATE (:R {name: "Bldr"})-[:GRANTED {slot: "08:00-20:00"}]->(:D {name: "GenOff"})'
     loading = false
-    txInfoHeaders = [
+    txHeaders = [
         {
             text: 'Transaction information',
             align: 'left',
@@ -70,44 +77,58 @@ export default class Communicator extends Vue {
             value: 'name',
         },
     ]
-    txProperties: { key: string; value: string }[] = []
-    keys: Blockchain.KeyPair = {
-        publicKey: process.env.VUE_APP_EXONUM_PUBLIC_KEY,
-        secretKey: process.env.VUE_APP_EXONUM_PRIVATE_KEY,
-    }
+
+    txData: { key: string; value: string }[] = []
 
     sendTx() {
         this.loading = true
-        Blockchain.sendTx(this.cypher, this.keys).then(({ tx_hash }) => {
-            this.txProperties = [
-                {
-                    key: 'Hash',
-                    value: tx_hash,
-                },
-                {
-                    key: 'Sender public key',
-                    value: this.keys.publicKey,
-                },
-                {
-                    key: 'Status',
-                    value: 'PENDING',
-                },
-                {
-                    key: 'Content',
-                    value: this.cypher,
-                },
-                {
-                    key: 'Error',
-                    value: '',
-                },
-            ]
-            this.cypher = ''
-            this.loading = true
-        })
-    }
+        Blockchain.sendTx(this.cypher, this.keys)
+            .then(({ tx_hash }) => {
+                this.txData = [
+                    {
+                        key: 'Hash',
+                        value: tx_hash,
+                    },
+                    {
+                        key: 'Sender public key',
+                        value: this.keys.publicKey,
+                    },
+                    {
+                        // HARDCODED, it must be in the 3rd place
+                        key: 'Status',
+                        value: 'PENDING',
+                    },
+                    {
+                        key: 'Content',
+                        value: this.cypher,
+                    },
+                    {
+                        key: 'Error',
+                        value: '',
+                    },
+                ]
 
-    mounted() {
-        console.log(process.env)
+                this.cypher = ''
+                return tx_hash
+            })
+            .then(tx_hash => delay(1000, tx_hash))
+            .then(tx_hash => Blockchain.getTx(tx_hash))
+            .then(value => {
+                // console.log(value)
+
+                if (!('status' in value)) {
+                    return
+                }
+
+                this.txData[2].value = (value.status
+                    .type as string).toUpperCase()
+                this.txData.push({
+                    key: 'Location',
+                    value: `Block ${value.location.block_height}`,
+                })
+
+                this.loading = false
+            })
     }
 }
 </script>
--- explorer/src/components/History.vue
+++ explorer/src/components/History.vue
@@ -1,7 +1,7 @@
 <template>
     <v-layout>
         <v-layout row wrap>
-            <v-flex xs8 offset-xs2 class="pt-5">
+            <v-flex xs10 offset-xs1 class="pt-5">
                 <v-text-field
                     label="Search"
                     solo
@@ -13,12 +13,19 @@
                 ></v-text-field>
                 <v-divider></v-divider>
                 <v-card>
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">
+                                Node History {{ shrink(query) }}
+                            </h3>
+                        </div>
+                    </v-card-title>
                     <v-data-table
                         :headers="modificationHeaders"
                         :items="modifications"
                         class="elevation-1"
                         hide-actions
-                        :loading="loading"
+                        :loading="loading.history"
                     >
                         <v-progress-linear
                             v-slot:progress
@@ -26,9 +33,49 @@
                             indeterminate
                         ></v-progress-linear>
                         <template v-slot:items="props">
-                            <td>{{ props.item.tx }}</td>
+                            <td>
+                                <v-btn
+                                    depressed
+                                    small
+                                    v-on:click.native="
+                                        getTx(props.item.transaction_id)
+                                    "
+                                    >{{
+                                        shrink(props.item.transaction_id)
+                                    }}</v-btn
+                                >
+                            </td>
+                            <td
+                                class="text-xs-left"
+                                v-html="props.item.description"
+                            ></td>
+                        </template>
+                    </v-data-table>
+                </v-card>
+                <v-divider></v-divider>
+                <v-card>
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">
+                                Tx {{ shrink(tx_hash) }}
+                            </h3>
+                        </div>
+                    </v-card-title>
+                    <v-data-table
+                        :items="txData"
+                        class="elevation-1"
+                        hide-actions
+                        :loading="loading.tx"
+                    >
+                        <v-progress-linear
+                            v-slot:progress
+                            color="blue"
+                            indeterminate
+                        ></v-progress-linear>
+                        <template v-slot:items="props">
+                            <td>{{ props.item.key }}</td>
                             <td class="text-xs-left">
-                                {{ props.item.description }}
+                                {{ props.item.value }}
                             </td>
                         </template>
                     </v-data-table>
@@ -42,35 +89,95 @@
 import { Vue, Component } from 'vue-property-decorator'
 
 import * as Blockchain from '../api/blockchain'
+import { shorten } from '../api/convenience'
 
 @Component({})
 export default class History extends Vue {
+    shrink(hash: string) {
+        return shorten(hash)
+    }
+
     query = ''
 
     modificationHeaders = [
         {
-            text: 'Node ~~~',
+            text: `Transaction`,
             align: 'left',
             sortable: false,
             value: 'name',
         },
         {
-            text: '',
+            text: 'Decription',
             align: 'left',
             sortable: false,
             value: 'name',
         },
     ]
-    modifications: { tx: string; description: string }[] = []
-    loading = false
+
+    modifications: Blockchain.NodeModification[] = []
+
+    loading = { history: false, tx: false }
 
     search() {
-        this.loading = true
+        this.loading.history = true
 
         Blockchain.getNodeHistory(this.query).then(history => {
-            console.log(history)
+            this.modifications = history.map(m => {
+                return {
+                    description: m.description.replace(
+                        /<b>([a-f0-9_]+)<\/b>/gi,
+                        (full, group) => {
+                            // console.log(group)
+                            return `<b>${shorten(group)}</b>`
+                        },
+                    ),
+                    transaction_id: m.transaction_id,
+                } as Blockchain.NodeModification
+            })
 
-            this.loading = false
+            this.loading.history = false
+        })
+    }
+
+    tx_hash = ''
+
+    txData: { key: string; value: string }[] = []
+
+    getTx(tx_hash: string) {
+        this.tx_hash = tx_hash
+        this.loading.tx = true
+
+        Blockchain.getTx(tx_hash).then(value => {
+            // console.log(value)
+
+            this.txData = [
+                {
+                    key: 'Hash',
+                    value: tx_hash,
+                },
+                {
+                    key: 'Sender public key',
+                    value: value.content.body.pub_key as string,
+                },
+                {
+                    key: 'Status',
+                    value: (value.status.type as string).toUpperCase(),
+                },
+                {
+                    key: 'Content',
+                    value: value.content.body.queries as string,
+                },
+                {
+                    key: 'Error',
+                    value: '',
+                },
+                {
+                    key: 'Location',
+                    value: `Block ${value.location.block_height}`,
+                },
+            ]
+
+            this.loading.tx = false
         })
     }
 }
