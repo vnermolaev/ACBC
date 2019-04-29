10b21e737a4e8660ed07e15400d5922be1305eba
quicky84 <victor.ermolaev@protonmail.com>
2019-04-29 15:02:34 +0200

    Update startup function

--- docker/unix_startup.sh
+++ docker/unix_startup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-image_name="exonum_neo4j"
+image_name="acbc"
 volume_name="ExonumConfVolume"
 node_count=4
 api_port=8200
@@ -15,33 +15,33 @@ for i in $(seq 1 $((node_count)))
 do
     echo docker run -t -d -i --name node$i -p 820$i:8200 -p 747$i:7474 -p 768$i:7687 -p 300$i:3005 -v $volume_name:/shared-config $image_name
     docker run -t -d -i --name node$i -p 820$i:8200 -p 747$i:7474 -p 768$i:7687 -p 300$i:3005 -v $volume_name:/shared-config $image_name
-    docker exec -w /Exonum-Neo4j/ node$i git pull
+    docker exec -w /ACBC/ node$i git pull
     docker exec node$i neo4j-admin set-initial-password exonumNeo4j
     docker exec node$i neo4j start
 done
 
 #cleaning shared_config folder and making common conf
-docker exec -w /Exonum-Neo4j/backend/ node1 ./configure/generate_common_config.sh $node_count $target
+docker exec -w /ACBC/backend/ node1 ./configure/generate_common_config.sh $node_count $target
 
 #Generating conf for each node
 for i in $(seq 1 $((node_count)))
 do
     #docker exec -w /Exonum-Neo4j/backend/ node$i ./reConfTestNode.sh $i $target
-    docker exec -w /Exonum-Neo4j/backend/ node$i ./configure/configure_node.sh $i $target
+    docker exec -w /ACBC/backend/ node$i ./configure/configure_node.sh $i $target
 done
 
 #Finalizing conf for each node
 for i in $(seq 1 $((node_count)))
 do
     #docker exec -w /Exonum-Neo4j/backend/ node$i ./finalizeTesting.sh $i $node_count $target
-    docker exec -w /Exonum-Neo4j/backend/ node$i ./configure/finalize_config.sh $i $node_count $target
+    docker exec -w /ACBC/backend/ node$i ./configure/finalize_config.sh $i $node_count $target
 done
 
 #Run all the nodes
 for i in $(seq 1 $((node_count)))
 do
-    docker exec -d -w /Exonum-Neo4j/backend/ node$i ./runTestNode.sh $i $target
-    docker exec -d -w /Exonum-Neo4j/backend/ node$i ./runTestNode.sh $i $target
+    # docker exec -d -w /Exonum-Neo4j/backend/ node$i ./runTestNode.sh $i $target
+    docker exec -d -w /ACBC/backend/ node$i ./configure/run_node.sh $i $target
 done
 
 #Wait so that neo4j has properly started
@@ -51,6 +51,6 @@ sleep 5
 for i in $(seq 1 $((node_count)))
 do
     # docker exec -w /Exonum-Neo4j/frontend/ node$i ./genEnv.sh $i localhost
-    docker exec -w /Exonum-Neo4j/explorer/ node$i ./generate_environment.sh $i localhost
-    docker exec -d -w /Exonum-Neo4j/explorer/ node$i npm start
+    docker exec -w /ACBC/explorer/ node$i ./generate_environment.sh $i localhost
+    docker exec -d -w /ACBC/explorer/ node$i npm run go
 done
