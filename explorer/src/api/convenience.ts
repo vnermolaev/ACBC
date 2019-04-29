ca60c7741a9accc1330c276c6fe69b8f4384a2ec
quicky84 <victor.ermolaev@protonmail.com>
2019-04-29 13:57:26 +0200

    Add convenience functions

---
+++ explorer/src/api/convenience.ts
@@ -0,0 +1,8 @@
+const shorten = (hash: string): string => hash.length > 11
+    ? `${hash.substring(0, 6)}~~${hash.substring(hash.length - 3)}`
+    : hash
+
+const delay = <T>(ms: number, value: T): Promise<T> =>
+    new Promise(resolve => setTimeout(() => resolve(value), ms))
+
+export { shorten, delay }
