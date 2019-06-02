2129a35cec7957d112033be0078097088c35d855
quicky84 <victor.ermolaev@protonmail.com>
2019-06-02 15:55:58 +0200

    update env script

--- explorer/generate_environment.sh
+++ explorer/generate_environment.sh
@@ -7,12 +7,13 @@ echo "WEBSERVER_PORT=3005" > .env
 echo "NEO4J_BOLT_ADDRESS=$ip" >> .env
 echo "NEO4J_BOLT_PORT=768$node" >> .env
 echo "NEO4J_USERNAME=neo4j" >> .env
-echo "NEO4J_PASSWORD=exonumNeo4j" >> .env
+echo "NEO4J_PASSWORD=ne4j" >> .env
 echo "EXONUM_ADDRESS=172.17.0.$((node+1))" >> .env
 echo "EXONUM_PORT=8200" >> .env
 
+SK=$(realpath ../backend/sec_$node.toml)
 prefix="VUE_APP_"
 printf "${prefix}EXONUM_PRIVATE_KEY=" >> .env
-grep -Po 'service_secret_key = "\K[^"]*' ../backend/sec_$node.toml >> .env
+grep -Po 'service_secret_key = "\K[^"]*' $SK >> .env
 printf "${prefix}EXONUM_PUBLIC_KEY=" >> .env
-grep -Po 'public_key = "\K[^"]*' ../backend/sec_$node.toml >> .env
+grep -Po 'public_key = "\K[^"]*' $SK >> .env
